92733aadea13e1c0425b203e0b64b95e98bd7ee9
Mare <voz.15.1998@gmail.com>
2018-05-29 23:19:17 +0300

    reworks + media queries

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-$('.slider').slick({
-    infinite: true,
-    arrows:true,
-    slidesToShow: 5,
-    slidesToScroll: 1
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log("DOM fully loaded and parsed");
+    $('.slider').slick({
+        infinite: true,
+        arrows: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
